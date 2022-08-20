b29710609d3f1534ac3c69a7831f82a24b4858c4
Chris Yang <kimisme9386@gmail.com>
2022-08-20 11:02:10 +0800

    fix: terraform format

--- main.tf
+++ main.tf
@@ -1,32 +1,32 @@
 terraform {
-   backend "remote" {
-     organization = "9incloud"
+  backend "remote" {
+    organization = "9incloud"
 
-      workspaces {
-        name = "dev-workspace"
-      }
-   }
+    workspaces {
+      name = "dev-workspace"
+    }
+  }
 }
 
 variable "awsprops" {
-    type = map
-    default = {
-      region = "ap-northeast-1"
-      profile = "dev"
-      vpc = "vpc-0e30a506ba92b3912"
-      ami = "ami-0ecb2a61303230c9d"
-      itype = "t2.micro"
-      subnet = "subnet-0c3348ffc23de5584"
-      publicip = true
-      keyname = "myseckey"
-      secgroupname = "IAC-Sec-Group"
-   }
+  type = map(any)
+  default = {
+    region       = "ap-northeast-1"
+    profile      = "dev"
+    vpc          = "vpc-0e30a506ba92b3912"
+    ami          = "ami-0ecb2a61303230c9d"
+    itype        = "t2.micro"
+    subnet       = "subnet-0c3348ffc23de5584"
+    publicip     = true
+    keyname      = "myseckey"
+    secgroupname = "IAC-Sec-Group"
+  }
 }
 
 provider "aws" {
   region = lookup(var.awsprops, "region")
   # profile only for local
-#   profile = lookup(var.awsprops, "profile")
+  #   profile = lookup(var.awsprops, "profile")
 }
 
 module "vpc" {
@@ -36,31 +36,31 @@ module "vpc" {
 
 
 resource "aws_security_group" "project-iac-sg" {
-  name = lookup(var.awsprops, "secgroupname")
+  name        = lookup(var.awsprops, "secgroupname")
   description = lookup(var.awsprops, "secgroupname")
-  vpc_id = lookup(var.awsprops, "vpc")
+  vpc_id      = lookup(var.awsprops, "vpc")
 
   // To Allow SSH Transport
   ingress {
-    from_port = 22
-    protocol = "tcp"
-    to_port = 22
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   // To Allow Port 80 Transport
   ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   lifecycle {
@@ -70,11 +70,11 @@ resource "aws_security_group" "project-iac-sg" {
 
 
 resource "aws_instance" "project-iac" {
-  ami = lookup(var.awsprops, "ami")
-  instance_type = lookup(var.awsprops, "itype")
-  subnet_id = lookup(var.awsprops, "subnet") #FFXsubnet2
+  ami                         = lookup(var.awsprops, "ami")
+  instance_type               = lookup(var.awsprops, "itype")
+  subnet_id                   = lookup(var.awsprops, "subnet") #FFXsubnet2
   associate_public_ip_address = lookup(var.awsprops, "publicip")
-#   key_name = lookup(var.awsprops, "keyname")
+  #   key_name = lookup(var.awsprops, "keyname")
 
 
   vpc_security_group_ids = [
@@ -82,13 +82,13 @@ resource "aws_instance" "project-iac" {
   ]
 
   tags = {
-    Name ="SERVER01"
+    Name        = "SERVER01"
     Environment = "DEV"
-    OS = "UBUNTU"
-    Managed = "IAC"
+    OS          = "UBUNTU"
+    Managed     = "IAC"
   }
 
-  depends_on = [ aws_security_group.project-iac-sg ]
+  depends_on = [aws_security_group.project-iac-sg]
 }
 
 
